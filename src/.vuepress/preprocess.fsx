963d571013adfae5882c37d7ab2ed4a5bc370e45
LazuliKao <gaoxinhong2004@hotmail.com>
2023-06-05 18:49:56 +0800

    Update preprocess.fsx

--- src/.vuepress/preprocess.fsx
+++ src/.vuepress/preprocess.fsx
@@ -6,6 +6,6 @@ let processGiscus()=
     let giscus = Path.Combine(node_modules,"giscus","dist","giscus.mjs")
     let data=File.ReadAllText(giscus)
     let result=data.Replace("""location.pathname.substring(1).replace(/\.\w+$/, "")""",
-        """location.pathname.substring(1).replace(/\.\w+$/, "").replace(/\.\w+$/, "").replace(/^(PFLPDocs)/g, "-")""")
+        """location.pathname.substring(1).replace(/\.\w+$/, "").replace(/\.\w+$/, "").replace(/^(PFLPDocs)\//g, "")""")
     File.WriteAllText(giscus,result)
 processGiscus()

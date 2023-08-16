85d76901730fd513d1d3c6f60c5660456d8da7e3
LazuliKao <lazulikao@nbt.edu.cn>
2023-08-16 19:41:54 +0800

    dark theme

--- pages/download/src/App.vue
+++ pages/download/src/App.vue
@@ -2,8 +2,19 @@
 import DownloadPage from '@/pages/DownloadPage.vue'
 </script>
 <template>
-  <main>
+  <main class="main">
     <download-page />
   </main>
 </template>
-<style scoped></style>
+<style scoped>
+.main::after {
+  background-color: black;
+  content: '';
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  z-index: -10;
+}
+</style>
